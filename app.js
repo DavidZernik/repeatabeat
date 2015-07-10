f79e60ffef6f662d825d785bd14e601f0b6678dd
David Zernik <dzernik@gmail.com>
2015-07-09 23:26:48 -0700

    first commit

--- app.js
+++ app.js
@@ -1,121 +1,281 @@
-//----------Toggle the columns ----------------------------------------------------------------->
-$(
-  function(){
-    $("#melodyHeader").click(function(){
-        $("#melodyOptions").slideToggle("medium");
-    });
-  }
-);
 
-//jQuery selector for the instrument column
-$(
-  function(){
-    $("#instrumentHeader").click(function(){
-        $("#instrumentOptions").slideToggle("medium");
-    });
-  }
-);
+//variable declarations----------------------------------------------------------------------------
 
-//jQuery selector for the beat column
-//first tag is what calls it, then what is changed
-$(
-  function(){
-    $("#beatHeader").click(function(){
-        $("#beatOptions").slideToggle("medium");
-    });
-  }
-);
-//---------------- jQuery call to the audio files ---------------------------------------------------------------
-// Original beat (audioOriginal)
-$(
-  function(){
-    $('#originalBeat').click(function(){
-        $("#audioOriginal1").trigger('play');
+//takes two elements, first element is player name, second element is score
+var currentPlayer;
+var player1 = {
+      name:["player 1"],
+      score: 0
+};
+
+var player2 = {
+      name:["player 2"],
+      score: 0
+};
+currentPlayer = player1;
+
+//creating an empty object
+var gameBoard = {};
+
+//Declare the gameBoard variables
+// First element is the overall originalBeat number
+// Second element is the melody number
+// Third element is the instrument number
+// Fourth element is the beat number
+gameBoard[0]= ["melody2", "instrument10", "beat11"];  //this means: originalBeat1 has the answer of: melody2, instrument5, beat1
+gameBoard[1]= ["melody3", "instrument8", "beat15"];
+gameBoard[2]= ["melody5", "instrument9", "beat12"];
+gameBoard[3]= ["melody4", "instrument7", "beat13"];
+
+var gameBoardArray = [gameBoard[0], gameBoard[1], gameBoard[2], gameBoard[3]];
+var actualGameBoard = [];
+var answerGuess = [];
+
+//function declarations-----------------------------------------------------------------------------------
+
+
+  //Toggle the columns
+  $(
+    function(){
+      $("#melodyHeader").click(function(){
+          $("#melodyOptions").slideToggle("medium");
       });
     }
-);
+  );
 
-// Melody sound 1 (audio1)
-$(
-  function(){
-    $('#melody1').click(function(){
-        $("#audio1").trigger('play');
+  //jQuery selector for the instrument column
+  $(
+    function(){
+      $("#instrumentHeader").click(function(){
+          $("#instrumentOptions").slideToggle("medium");
       });
     }
-);
+  );
 
-// Melody sound 1 (audio1)
-$(
-  function(){
-    $('#audio2').click(function(){
-        $("#melody2").trigger('play');
+  //jQuery selector for the beat column
+  //first tag is what calls it, then what is changed
+  $(
+    function(){
+      $("#beatHeader").click(function(){
+          $("#beatOptions").slideToggle("medium");
       });
     }
-);
+  );
+  //jQuery audio to button for melody
+  $(
+    function(){
+      $('#originalBeat').click(function(){
+          $("#audioOriginal1").trigger('play');
+        });
+      }
+  );
 
-// Melody sound 1 (audio1)
-$(
-  function(){
-    $('#melodyOne').click(function(){
-        $("#melody3").trigger('play');
-      });
+  $(
+    function(){
+      $('#melody1').click(function(){
+          $("#audio1").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#melody2').click(function(){
+          $("#audio2").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#melody3').click(function(){
+          $("#audio3").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#melody4').click(function(){
+          $("#audio4").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#melody5').click(function(){
+          $("#audio5").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#instrument6').click(function(){
+          $("#audio6").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#instrument7').click(function(){
+          $("#audio7").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#instrument7').click(function(){
+          $("#audio7").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#instrument8').click(function(){
+          $("#audio8").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#instrument9').click(function(){
+          $("#audio9").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#instrument10').click(function(){
+          $("#audio10").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#beat11').click(function(){
+          $("#audio11").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#beat12').click(function(){
+          $("#audio12").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#beat13').click(function(){
+          $("#audio13").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#beat14').click(function(){
+          $("#audio14").trigger('play');
+        });
+      }
+  );
+
+  $(
+    function(){
+      $('#beat15').click(function(){
+          $("#audio15").trigger('play');
+        });
+      }
+  );
+//double click event for melody
+//I made a mistake that this eventListener was inside of a function, don't do that again
+//Also,  use the document(ready) jquery format instead, otherwise I have to have the JavaScript
+// at the bottom of the file.
+
+  $('button.melody').on('dblclick',function(){
+    //"This" is referring to the button on the line above, and .id is how you call its id
+    //When the button is double clicked, push it to the array "answerGuess"
+    answerGuess.push(this.id);
+    if (answerGuess[0] === gameBoard[0][0]){
+      alert("You guessed the right melody! Now double click on the instrument that matches the original beat This is part of buttonmelody eventlistener");
     }
-);
-
-// Melody sound 1 (audio1)
-$(
-  function(){
-    $('#melodyOne').click(function(){
-        $("#melody4").trigger('play');
-      });
+    else{
+      alert("Sorry, wrong guess..next player start");
+      //change player because current player lost
+      setTurn();
+      //empty the array for the next player, this needs to be before 'return'
+      answerGuess = [];
+      //empty the array for the next player
+      //Is it not ok to call a universal variable inside a function?
     }
-);
+  });
 
-// Melody sound 1 (audio1)
-$(
-  function(){
-    $('#melodyOne').click(function(){
-        $("#melody5").trigger('play');
-      });
+  $('button.instrument').dblclick(function(){
+    //"This" is referring to the button on the line above, and .id is how you call its id
+    //When the button is double clicked, push it to the array "answerGuess"
+    answerGuess.push(this.id);
+    if (answerGuess[1] === gameBoard[0][1]){
+      alert("You guessed the right instrument! Now double click on the instrument that matches the original song");
     }
-);
-// --------------------------------------------------------------------------------------------------------
-//Declare the different beats, there will be 3 total
-//each element in the array is the correct button which corresponds
-//to that beat, i.e. [1,1,1] means the first melody button, the first
-//instrument button and the first drums button
-var beat1 = [1,1,1];
-var beat2 = [2,2,2];
-var beat3 = [3,3,3];
+    else{
+      alert("Sorry, wrong guess..next player start");
+      answerGuess = [];
+      //change player because current player lost
+      setTurn();
+    }
+  });
 
-//Declare the original beat
-var originalBeat;
+$('button.beat').dblclick(function(){
+    //"This" is referring to the button on the line above, and .id is how you call its id
+    //When the button is double clicked, push it to the array "answerGuess"
+    answerGuess.push(this.id);
+    if (answerGuess[2] === gameBoard[0][2]){
+      //alert the user they were right
+      alert("Good ear! You got it all right! Point. Now it's the next player's turn");
+      //add a point to currentPlayer's score
+      currentPlayer.score++;
+      answerGuess = [];
+      //change the scoreboard
+      if (currentPlayer == player1){
+        $("#player1Score").html("player 1 has " + currentPlayer.score);
+        if (currentPlayer.score == 4){
+          alert("Game over, " + currentPlayer.name + "won!");
+        }
+      }
+      else {
+        $("#player2Score").html("player 2 has " + currentPlayer.score);
+      }
+      //change players
+      setTurn();
+    }
+    else {
+      //alert the user it was wrong
+      alert("Sorry, wrong guess..next player start");
+      //re-set the gueser board
+      answerGuess = [];
+      //change players
+      setTurn();
+    }
+  });
 
-
-//  Game alerts whose turn it is  -done
-//  Game randomly picks an original beat  -done
-//  Game assigns that original beat to the correct button -done
-//  Player can click the original beat to hear it -done
-//  Player selects which melody he thinks he heard
-//  Player can play each melody, instrument, and drum file to hear it
-//  Player must guess if the meldoy, instrument and drum files
-//  match the original beat.
-function playersTurn(){
-  // 1 here will mean player 1's turn
-  var whoseTurn = 1;
-  alert("It is now player " + whoseTurn +"'s turn");
-  //randomly choose a beat
-  var whichBeat = Math.floor((Math.random() * 3) + 1);
-  //now assign that number to the correct original beat
-  if (whichBeat === 1){
-    originalBeat = beat1;
+//---------------------------------------------------------------------------------------------------------------
+  //set whose turn it is next by changing currentPlayer
+  function setTurn(){
+    console.log(currentPlayer[0]);
+    if ( currentPlayer.name == ("player 1")){
+      currentPlayer = player2;
+    }
+    else {
+      currentPlayer = player1;
+    }
   }
-  else if (whichBeat === 2){
-    originalBeat = beat2;
-  }
-  else {
-    originalBeat = beat3;
-  }
-  console.log(originalBeat);
-  //jQuery to play the audio
-}
